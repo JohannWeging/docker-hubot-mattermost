50ea951a111834097c31b101a4f1a12c79eaddfc
Johann Weging [OPS] <johann@weging.com>
2017-10-23 19:13:54 +0200

    remove npm deps

--- rootfs/hubot.sh
+++ rootfs/hubot.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-
-if [ ${DEBUG} = "true" ]; then
+if [ "${DEBUG}" == "true" ]; then
     set -x
 fi
 
-HUBOT_NAME=${HUBOT_NAME:-"hubot"}
-HUBOT_DESCIPTION=${HUBOT_DESCIPTION:-"hubot bot"}
+HUBOT_NAME=${HUBOT_NAME:-"bot"}
+HUBOT_DESCIPTION=${HUBOT_DESCIPTION:-"'hubot bot'"}
 HUBOT_EXTERNAL_SCRIPTS=${HUBOT_EXTERNAL_SCRIPTS:-""}
 HUBOT_NPM_DEPS=${HUBOT_NPM_DEPS:-""}
 
@@ -29,9 +28,13 @@ if [ -z ${MATTERMOST_PASSWORD+x} ]; then
     exit 1
 fi
 
+if [ "${HUBOT_NAME}" == "hubot" ]; then
+    echo "HUBOT_NAME: 'hubot' is not allowed, the installation would fail"
+    exit 1
+fi
 cd /hubot || exit 1
 
-echo "No"| gosu hubot yo hubot --adapter matteruser --owner="${HUBOT_OWNER}" --name="${HUBOT_NAME}" --description="${HUBOT_DESCIPTION}" --defaults
+echo "No"| gosu hubot yo hubot --adapter matteruser --owner "${HUBOT_OWNER}" --name "${HUBOT_NAME}" --description "${HUBOT_DESCIPTION}" --defaults
 
 echo $HUBOT_EXTERNAL_SCRIPTS | jq -R 'gsub("( |\t)+"; "") | split(",")' > external-scripts.json
 
@@ -40,7 +43,7 @@ if [ -n "${FILE_BRAIN_PATH+x}" ]; then
     chown hubot:hubot ${FILE_BRAIN_PATH}
 fi
 
-DEPS=$(echo ${HUBOT_NPM_DEPS} | tr -d ' \t\n')
+DEPS=$(echo ${HUBOT_EXTERNAL_SCRIPTS} | tr -d ' \t\n')
 
 OIFS=${IFS}
 IFS=','
